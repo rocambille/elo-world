e47932fcbe9e07a63e6b310ff1490641fb9e0b7a
romaing <rocambille@gmail.com>
2021-08-03 15:09:34 +0200

    added deployment through gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import { LoginDataProvider, MovieListProvider } from './contexts';
 import Controls from './components/Controls';
